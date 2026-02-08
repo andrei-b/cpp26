a408049c6e78444c0d111fd3bac29d2b027e6a7e
andrei_b <andrei.borovsky@gmail.com>
2026-02-08 18:33:54 +0100

    functions & params

--- demos/reflection/main.cpp
+++ demos/reflection/main.cpp
@@ -3,12 +3,17 @@
 #include <array>
 #include <cstddef>
 #include <cstdint>
+#include <string_view>
 
 struct Person {
     int age;
     const double height;
     int add(int a, int b) { return a + b; }
     static  int foo() { return 42; }
+    int test_call(int i) {
+        std::cout << "test_call called with " << i << "\n";
+        return i;
+    }
 };
 
 static void print_u8(std::u8string_view s) {
@@ -133,6 +138,50 @@ static void print_flags(std::uint32_t f) {
     if (f & MF_STATIC) add ("static");
 }
 
+template <typename T>
+consteval std::meta::info find_member_function(std::u8string_view wanted) {
+    for (std::meta::info m : std::meta::members_of(^^T, std::meta::access_context::current())) {               // GCC branch uses ^^
+        if (std::meta::is_function(m) && std::meta::u8identifier_of(m) == wanted) {
+            return m;
+        }
+    }
+    // You can also std::terminate or throw; error handling varies by implementation
+    throw "member function not found";
+}
+
+template <std::size_t N>
+struct fixed_string {
+    char8_t value[N];
+
+    consteval fixed_string(const char (&s)[N]) {
+        for (std::size_t i = 0; i < N; ++i) value[i] = static_cast<char8_t>(s[i]);
+    }
+
+    consteval fixed_string(const char8_t (&s)[N]) {
+        for (std::size_t i = 0; i < N; ++i) value[i] = s[i];
+    }
+
+    constexpr std::u8string_view view() const noexcept {
+        return std::u8string_view(value, N - 1); // drop terminating '\0'
+    }
+
+    constexpr bool operator==(fixed_string const&) const = default;
+};
+
+
+template <std::size_t N>
+fixed_string(const char (&)[N]) -> fixed_string<N>;
+
+template <fixed_string Name, class T, class... Args>
+decltype(auto) call_reflected(T&& obj, Args&&... args) {
+    using U = std::remove_reference_t<T>;
+    constexpr std::meta::info mem = find_member_function<U>(Name.view());
+
+    // splice the member function name into the call:
+    return std::forward<T>(obj).[:mem:](std::forward<Args>(args)...);
+}
+
+
 int main() {
     std::cout << "Person members:\n";
     for (std::size_t i = 0; i < ReflectedMembers<Person>::count
@@ -164,4 +213,6 @@ int main() {
         }
         std::cout << "\n";
     }
+    Person p{};
+    std::cout << "function returns " << call_reflected<"test_call">(p, 42);
 }
