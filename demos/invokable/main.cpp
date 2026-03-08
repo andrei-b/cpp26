d2379edf2d1ad4b265bd8156f7bed9f89731d512
andrei <andrei.borovsky@gmail.com>
2026-03-08 11:42:47 +0100

    added invoker

--- demos/invokable/main.cpp
+++ demos/invokable/main.cpp
@@ -24,13 +24,38 @@ protected:
     auto make_invoker(C* c, std::string_view method_name) {
         using traits = function_traits<C, Sig>;
         using R = typename traits::return_type;
+        using args_tuple = typename traits::args_tuple;
 
-        if constexpr (std::tuple_size_v<typename traits::args_tuple> == 2) {
-            using Arg1 = std::tuple_element_t<0, typename traits::args_tuple>;
-            using Arg2 = std::tuple_element_t<1, typename traits::args_tuple>;
-            return make_invoker<C, R, Arg1, Arg2>(c, method_name);
+        constexpr auto arg_count = std::tuple_size_v<args_tuple>;
+
+        if constexpr (arg_count == 0) {
+            return make_invoker_impl<C, R>(c, method_name);
         }
-        throw std::runtime_error("Unsupported signature");
+        else if constexpr (arg_count == 1) {
+            using Arg1 = std::tuple_element_t<0, args_tuple>;
+            return make_invoker_impl<C, R, Arg1>(c, method_name);
+        }
+        else if constexpr (arg_count == 2) {
+            using Arg1 = std::tuple_element_t<0, args_tuple>;
+            using Arg2 = std::tuple_element_t<1, args_tuple>;
+            return make_invoker_impl<C, R, Arg1, Arg2>(c, method_name);
+        }
+        else if constexpr (arg_count == 3) {
+            using Arg1 = std::tuple_element_t<0, args_tuple>;
+            using Arg2 = std::tuple_element_t<1, args_tuple>;
+            using Arg3 = std::tuple_element_t<2, args_tuple>;
+            return make_invoker_impl<C, R, Arg1, Arg2, Arg3>(c, method_name);
+        }
+        else if constexpr (arg_count == 4) {
+            using Arg1 = std::tuple_element_t<0, args_tuple>;
+            using Arg2 = std::tuple_element_t<1, args_tuple>;
+            using Arg3 = std::tuple_element_t<2, args_tuple>;
+            using Arg4 = std::tuple_element_t<3, args_tuple>;
+            return make_invoker_impl<C, R, Arg1, Arg2, Arg3, Arg4>(c, method_name);
+        }
+        else {
+            throw std::runtime_error("Unsupported signature - too many arguments");
+        }
     }
 
     template <typename T, typename Sig>
@@ -109,7 +134,8 @@ private:
 
     template <class C, typename R, typename... Args>
     std::function<R(Args... )> make_invoker_impl(C * c, std::string_view method_name) {
-        template for (constexpr auto m : reflected_members<C>())
+        constexpr auto ctx = std::meta::access_context::current();
+        template for (constexpr auto m : std::define_static_array(std::meta::members_of(^^C, ctx)))
         {
             if constexpr (std::meta::is_function(m) && std::meta::has_identifier(m))
             {
@@ -118,7 +144,7 @@ private:
                     if constexpr (std::meta::is_static_member(m))
                     {
                         auto fn = &[:m:];
-                        if constexpr (requires { fn(std::declval<Args>()...); }) {
+                        if constexpr (std::is_invocable_r_v<R, decltype(fn), Args...>) {
                             return [fn](Args... args) -> R {
                                 return fn(std::forward<Args>(args)...);
                             };
@@ -127,7 +153,7 @@ private:
                     else
                     {
                         auto pmf = &[:m:];
-                        if constexpr (requires { (std::declval<C>().*pmf)(std::declval<Args>()...); }) {
+                        if constexpr (std::is_invocable_r_v<R, decltype(pmf), C*, Args...>) {
                             return [c, pmf](Args... args) -> R {
                                 return (c->*pmf)(std::forward<Args>(args)...);
                             };
@@ -176,21 +202,43 @@ public:
 
         return call_method_impl_with_args<T, R, Args...>(*this, method_name, std::forward<Args>(args)...);
     }
+
+    template <typename Sig>
+    auto make_invoker(std::string_view method_name) {
+        return Invokable::make_invoker<TestClass, Sig>(this, method_name);
+    }
+
     ~TestClass() override = default;
 };
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    TestClass tc;
-    tc.call_method<void()>("hello");
-    int sum = tc.call_method<int(int, int)>("add", 3, 4);
-    auto d = tc.call_method<double(double, double, double, double)>("distance", 0.0, 0.0, 3.0, 4.0);
-    std::cout << "Sum: " << sum << "\n";
-    std::string str = tc.call_method<std::string(double, double)>("toString", 1.6, 2.5);
-    std::cout << "String: " << str << "\n";
-    std::cout << "Distance: " << d << "\n";
-    auto s1 = tc.call_method<int(int, int, int)>("add", 1, 2, 3);
-    std::cout << "Sum with 3 args: " << s1 << "\n";
-    return 0;
+    try {
+        TestClass tc;
+        tc.call_method<void()>("hello");
+        int sum = tc.call_method<int(int, int)>("add", 3, 4);
+        auto d = tc.call_method<double(double, double, double, double)>("distance", 0.0, 0.0, 3.0, 4.0);
+        std::cout << "Sum: " << sum << "\n";
+        std::string str = tc.call_method<std::string(double, double)>("toString", 1.6, 2.5);
+        std::cout << "String: " << str << "\n";
+        std::cout << "Distance: " << d << "\n";
+        auto s1 = tc.call_method<int(int, int, int)>("add", 1, 2, 3);
+        std::cout << "Sum with 3 args: " << s1 << "\n";
+
+        // Test make_invoker with overloaded methods
+        auto add2 = tc.make_invoker<int(int, int)>("add");
+        auto add3 = tc.make_invoker<int(int, int, int)>("add");
+
+        std::cout << "Invoker add2(5, 6): " << add2(5, 6) << "\n";
+        std::cout << "Invoker add3(5, 6, 7): " << add3(5, 6, 7) << "\n";
+
+        auto dist = tc.make_invoker<double(double, double, double, double)>("distance");
+        std::cout << "Invoker distance(0, 0, 5, 12): " << dist(0, 0, 5, 12) << "\n";
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
---
+++ demos/invokable/test_simple.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from test!" << std::endl;
+    std::cout << "Test 1" << std::endl;
+    std::cout << "Test 2" << std::endl;
+    return 0;
+}
+
